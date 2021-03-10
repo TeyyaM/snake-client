e05fd3a226c616874be53405ccc1e5a22fed4def
Tessa Azar MacLean <tessaamac@gmail.com>
2021-03-10 20:55:27 +0000

    Have a nickname, no movement yet, though!

--- client.js
+++ client.js
@@ -5,14 +5,48 @@ const net = require('net');
  */
 const connect = function () {
   const conn = net.createConnection({
-    host: '<IP ADDRESS HERE>',
+    // host: '135.23.222.131',
+    // port: 50542
+    host: 'localhost',
     port: 50541
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
-
+  // receive messages/date from server
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+  // Lets us know we connected, and sends the server our initials
+  conn.on('connect', () => {
+    conn.write('Name: T.M');
+    conn.write('Move: up');
+    console.log('Successfully connected to game server');
+    conn.write('Move: right');
+    conn.write('Move: up');
+    conn.write('Move: left');
+    setInterval(() => {
+      conn.write('Move: up');
+    }, 50);
+    // process.stdin.on('data', (message) => {
+    //   conn.write(message);
+    // });
+  });
   return conn;
 }
 
-module.exports = connect;
+// const moveSnake = () => {
+//   const net = require('net');
 
+// const conn = net.createConnection({
+//   host: 'localhost',
+//   port: 50541
+// });
+// process.stdin.on('data', (message) => {
+//   conn.write(message);
+// });
+// }
+
+module.exports = connect;
+// module.exports = {
+//   connect,
+// };
--- play.js
+++ play.js
@@ -1,4 +1,14 @@
 // play.js
-const { connect } = require('./client');
+const connect = require('./client');
 console.log('Connecting ...');
 connect();
+
+// const net = require('net');
+
+// const conn = net.createConnection({
+//   host: 'localhost',
+//   port: 50541
+// });
+// process.stdin.on('data', (message) => {
+//   conn.write(message);
+// });
